7e8c51067adf93dac1f2e49671a5fe2cbdb2a677
Maxim Muradov <koreantech620@mail.ru>
2024-06-05 22:06:33 +0700

    Add Task

--- src/test/java/tests/ZentistTest.java
+++ src/test/java/tests/ZentistTest.java
@@ -3,11 +3,12 @@ package tests;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.*;
 
 import static com.codeborne.selenide.Selenide.*;
-
+@Tag("ZentistTests")
 public class ZentistTest extends BaseTest
 {
     ScheduleDemo zentist  = new ScheduleDemo();
