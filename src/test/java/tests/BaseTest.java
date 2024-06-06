9ce43476e9b4cd44033fc11360e09f77a2f68160
Maxim Muradov <koreantech620@mail.ru>
2024-06-06 13:41:58 +0700

    Add refactor

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -18,8 +18,8 @@ public class BaseTest {
     @BeforeEach
     void setUp() {
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.baseUrl = "https://www.zentist.io/";
-        open("https://www.zentist.io/");
+        Configuration.baseUrl = "https://www.zentist.io/";
+        //open("https://www.zentist.io/");
         Configuration.remote = System.getProperty("Wdhost","https://user1:1234@selenoid.autotests.cloud/wd/hub");
         browser = System.getProperty("Browser","chrome");
         Configuration.browserVersion = System.getProperty("BrowserVersion","120.0");
